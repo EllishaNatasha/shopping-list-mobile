eaf0891d78b2b0171f84bb126f236ed5a7ad6a73
EllishaNatasha <ellishanatasha28@gmail.com>
2023-11-08 14:36:42 +0700

    tutorial 7 selesai

--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-
-
+import 'package:shopping_list/widgets/left_drawer.dart';
+import 'package:shopping_list/screens/shoplist_form.dart';
+import 'package:shopping_list/widgets/shop_card.dart';
 
 
 class MyHomePage extends StatelessWidget {
@@ -18,8 +19,11 @@ class MyHomePage extends StatelessWidget {
         title: const Text(
           'Shopping List',
         ),
-        backgroundColor:Colors.indigo ,
+        backgroundColor:Colors.indigo,
+        foregroundColor: Colors.white,
       ),
+      // Masukkan drawer sebagai parameter nilai drawer dari widget Scaffold
+      drawer: const LeftDrawer(),
       body: SingleChildScrollView(  
         // Widget wrapper yang dapat discroll
         child: Padding(
@@ -77,7 +81,18 @@ class ShopCard extends StatelessWidget {
             ..hideCurrentSnackBar()
             ..showSnackBar(SnackBar(
                 content: Text("Kamu telah menekan tombol ${item.name}!")));
+
+          // Navigate ke route yang sesuai (tergantung jenis tombol)
+          if (item.name == "Tambah Produk") {
+            if (item.name == "Tambah Produk") {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const ShopFormPage()));
+            }
+          }
+
         },
+
+        
         child: Container(
           // Container untuk menyimpan Icon dan Text
           padding: const EdgeInsets.all(8),
@@ -105,9 +120,3 @@ class ShopCard extends StatelessWidget {
   }
 }
 
-class ShopItem {
-  final String name;
-  final IconData icon;
-
-  ShopItem(this.name, this.icon);
-}
